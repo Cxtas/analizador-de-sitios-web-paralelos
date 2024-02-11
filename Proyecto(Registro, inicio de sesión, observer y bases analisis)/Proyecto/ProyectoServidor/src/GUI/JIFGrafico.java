c529a233e396271fc914f0514be441fff5ecdd20
Estephanie <92496437+Estephaniec@users.noreply.github.com>
2024-02-11 11:45:16 -0600

    gráfico comentado

--- Proyecto(Registro, inicio de sesión, observer y bases analisis)/Proyecto/ProyectoServidor/src/GUI/JIFGrafico.java
+++ Proyecto(Registro, inicio de sesión, observer y bases analisis)/Proyecto/ProyectoServidor/src/GUI/JIFGrafico.java
@@ -50,18 +50,21 @@ public class JIFGrafico extends javax.swing.JInternalFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jcbSitios = new javax.swing.JComboBox<>();
-        jbtnGraficar = new javax.swing.JButton();
+        jbtnGraficarPastel = new javax.swing.JButton();
         jbtnVolver = new javax.swing.JButton();
+        jbtnBarras = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setTitle("Estadísticas");
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel1.setText("Seleccione el sitio del cual desea ver su análisis");
 
-        jbtnGraficar.setText("Graficar");
-        jbtnGraficar.addActionListener(new java.awt.event.ActionListener() {
+        jbtnGraficarPastel.setText("Graficar");
+        jbtnGraficarPastel.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jbtnGraficarActionPerformed(evt);
+                jbtnGraficarPastelActionPerformed(evt);
             }
         });
 
@@ -72,6 +75,12 @@ public class JIFGrafico extends javax.swing.JInternalFrame {
             }
         });
 
+        jbtnBarras.setText("Graficar");
+
+        jLabel2.setText("Pastel");
+
+        jLabel3.setText("De barras");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -81,15 +90,24 @@ public class JIFGrafico extends javax.swing.JInternalFrame {
                 .addComponent(jLabel1)
                 .addGap(41, 41, 41))
             .addGroup(layout.createSequentialGroup()
-                .addGap(40, 40, 40)
-                .addComponent(jcbSitios, javax.swing.GroupLayout.PREFERRED_SIZE, 422, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(23, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(196, 196, 196)
-                .addComponent(jbtnGraficar)
+                .addGap(103, 103, 103)
+                .addComponent(jbtnGraficarPastel)
+                .addGap(51, 51, 51)
+                .addComponent(jbtnBarras)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jbtnVolver)
                 .addGap(31, 31, 31))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(40, 40, 40)
+                        .addComponent(jcbSitios, javax.swing.GroupLayout.PREFERRED_SIZE, 422, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(125, 125, 125)
+                        .addComponent(jLabel2)
+                        .addGap(82, 82, 82)
+                        .addComponent(jLabel3)))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -98,39 +116,44 @@ public class JIFGrafico extends javax.swing.JInternalFrame {
                 .addComponent(jLabel1)
                 .addGap(29, 29, 29)
                 .addComponent(jcbSitios, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jbtnGraficar)
-                    .addComponent(jbtnVolver))
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jbtnGraficarPastel)
+                    .addComponent(jbtnVolver)
+                    .addComponent(jbtnBarras))
                 .addGap(27, 27, 27))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jbtnGraficarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnGraficarActionPerformed
-        Sitio sitio= (Sitio)this.jcbSitios.getSelectedItem();
-
-        DefaultPieDataset pieDataset= new DefaultPieDataset();
-
-        pieDataset.setValue("Enlaces", sitio.getEnlaces());
-        pieDataset.setValue("Imagenes", sitio.getImagenes());
-        pieDataset.setValue("Videos", sitio.getVideos());
-        pieDataset.setValue("Titulos", sitio.getTitulos());
-        pieDataset.setValue("Subtitulos", sitio.getSubtitulos());
-        pieDataset.setValue("Tablas", sitio.getTablas());
-
-        JFreeChart chart= ChartFactory.createPieChart(
-            "Elecciones generales",
-            pieDataset,
-            true,
-            true,
-            false);
-
-        ChartFrame frame= new ChartFrame("Grafico circular", chart);
-        frame.pack();
-        frame.setVisible(true);
-    }//GEN-LAST:event_jbtnGraficarActionPerformed
+    private void jbtnGraficarPastelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnGraficarPastelActionPerformed
+//        Sitio sitio= (Sitio)this.jcbSitios.getSelectedItem();
+//
+//        DefaultPieDataset pieDataset= new DefaultPieDataset();
+//
+//        pieDataset.setValue("Enlaces", sitio.getEnlaces());
+//        pieDataset.setValue("Imagenes", sitio.getImagenes());
+//        pieDataset.setValue("Videos", sitio.getVideos());
+//        pieDataset.setValue("Titulos", sitio.getTitulos());
+//        pieDataset.setValue("Subtitulos", sitio.getSubtitulos());
+//        pieDataset.setValue("Tablas", sitio.getTablas());
+//
+//        JFreeChart chart= ChartFactory.createPieChart(
+//            "Elecciones generales",
+//            pieDataset,
+//            true,
+//            true,
+//            false);
+//
+//        ChartFrame frame= new ChartFrame("Grafico circular", chart);
+//        frame.pack();
+//        frame.setVisible(true);
+    }//GEN-LAST:event_jbtnGraficarPastelActionPerformed
 
     private void jbtnVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtnVolverActionPerformed
         this.setVisible(false);
@@ -139,7 +162,10 @@ public class JIFGrafico extends javax.swing.JInternalFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JButton jbtnGraficar;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JButton jbtnBarras;
+    private javax.swing.JButton jbtnGraficarPastel;
     private javax.swing.JButton jbtnVolver;
     private javax.swing.JComboBox<String> jcbSitios;
     // End of variables declaration//GEN-END:variables
